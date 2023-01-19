5994d8dc12dcba06a84a3cd5d2457b9efe43f8f5
Fernando Chiesa <104650078+firthunandz@users.noreply.github.com>
2023-01-19 16:10:24 -0300

    Add files via upload
    update index

--- DrinksWeb/index.php
+++ DrinksWeb/index.php
@@ -2,47 +2,40 @@
 
 
 <?php include("config/database.php"); 
-  $drinks = "SELECT nombre FROM drinks";
+  //tabla lista de tragos
+  $drinks = "SELECT * FROM drinks"; 
   $result = mysqli_query($conn, $drinks);
   $tragos = mysqli_fetch_all($result, MYSQLI_ASSOC);
-  
+
+  //tabla lista de ingredientes
+  $ingredients = "SELECT * FROM ingredients"; 
+  $result2 = mysqli_query($conn, $ingredients);
+  $ingredients2 = mysqli_fetch_all($result2, MYSQLI_ASSOC);
+
+  //tabla intermedia
+  $FKtable = "SELECT * FROM ingredient_drink";
+  $resultFK = mysqli_query($conn, $FKtable);
+  $intermedia = mysqli_fetch_all($resultFK, MYSQLI_ASSOC);
 ?>
 
-
-  <div class="col align-self-center">
-    <?php foreach($tragos as $trago){ ?>
+  <div class="row">
+    <div class="col sm-6">
+  <div class="col align-self-center s6 md3">
+    <?php foreach($tragos as $trago): ?>
     <div class="card text-center" style="width: 18rem;">
       <div class="card-body">
-      
         <h5 class="card-title"><?php echo htmlspecialchars($trago['nombre']); ?></h5>
-        <p class="card-text">Ingredients</p>
+        <p class="card-text">
+          
+          ingredientes
+      
+        </p>
         <a href="#" class="btn btn-primary">Image</a>
-
       </div>
     </div>
-    <?php }?>
+    <?php endforeach;?>
   </div>
-
-
-
-<!-- <div class="container">
-  <div class="row">
-
-    <?php foreach($tragos as $trago){ ?>
-
-    <div class="col s6 md3">
-      <div class="card z-depth-0">
-        <div class="card-content center">
-          <h6><?php echo htmlspecialchars($trago['nombre']); ?></h6>
-          <div>Ingredients</div>
-        </div>
-        <div class="card-action center">
-          <a href="#" class="brand-text">Image</a>
-        </div>
-      </div>
-    </div>
-      <?php }?>
   </div>
-</div> -->
+  </div>
 
 <?php include("template/pie.php"); ?>
